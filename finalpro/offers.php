dc7674faa50c299def89cf4806d080d05b560a81
Gulsher6862 <zshera6862@gmail.com>
2019-08-18 19:30:18 -0400

    added offers php file to db

---
+++ finalpro/offers.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>offers</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="offers.css">
+</head>
+<body>
+   <div class="container-fluid">
+  <div class="row">
+        <div class="col-lg-2">
+             <div class="logo">
+                 <img class="img-fluid" src="logo.png">
+             </div>
+        </div>
+        <div class="col-lg-6 offset-lg-4">
+           <ul class="nav">
+               <li class="nav-item">
+                      <a class="nav-link" href="#">OUR MENU</a>
+               </li>
+              
+               <li class="nav-item">
+                      <a class="nav-link" href="#">OFFERS</a>
+               </li>
+               <li class="nav-item">
+                      <a class="nav-link" href="#">CONTACT US</a>
+               </li>
+               <li class="nav-item">
+                      <a class="nav-link" href="#">ABOUT US</a>
+               </li>
+           </ul>
+        </div>
+        </div>
+   <div class="container">
+          <div class="row">
+          <div class="col-lg-6" id="img1">
+               <div class="text">
+               <h3>Buy 1 <span style="weight:bold;"> Get 1</h5>
+               <h1>Free</h1>
+               </div>
+          </div>
+          <div class="col-lg-6" id="img2">
+                <div class="text">
+                <h3>Get 30% off</h3>
+                </div>
+          </div>
+    </div>
+    <div class="row">
+          <div class="col-lg-12">
+                <div class="text1">
+                <h2>Visit Our Store and get 10% discount</h2>
+                </div>
+          </div>
+    </div>
+</body>
